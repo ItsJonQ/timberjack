aecebddd7963d378da82c95a718189ac4a14ba4a
ItsJonQ <itsjonq@gmail.com>
2014-12-14 01:08:09 -0500

    Removed adjusted wp_title for single template

--- single.php
+++ single.php
@@ -6,7 +6,6 @@
 $post = new LumberjackPost();
 $data = Timber::get_context();
 $data['post'] = $post;
-$data['wp_title'] .= ' - ' . $post->title();
 $data['comment_form'] = TimberHelper::get_comment_form();
 
 if (post_password_required($post->ID)){
