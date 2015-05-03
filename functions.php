21eadb11aa36e202346f0cacd08d657a77013d06
ItsJonQ <itsjonq@gmail.com>
2015-05-03 19:43:29 -0400

    Removed sample add to context

--- functions.php
+++ functions.php
@@ -41,9 +41,6 @@ class StarterSite extends TimberSite {
   }
 
   function add_to_context($context){
-    $context['foo'] = 'bar';
-    $context['stuff'] = 'I am a value set in your functions.php file';
-    $context['notes'] = 'These values are available everytime you call Timber::get_context();';
     $context['menu'] = new TimberMenu();
     $context['site'] = $this;
     return $context;
